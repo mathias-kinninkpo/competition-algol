0824f36bf4ca51436f2b1e9bfac221178dfab250
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-03 22:33:50 +0100

    max index

---
+++ test_gits/test_4.py
@@ -0,0 +1,11 @@
+"""
+    max_index : renvoie l'index du maximum
+    @tab : list[int]
+    @return : int - l'index du max
+"""
+def max_index(tab : list[int]) -> int:
+    
+    return tab.index(max(tab))
+
+
+
